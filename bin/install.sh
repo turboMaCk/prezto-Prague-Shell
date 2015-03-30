6ceb143bca7ea3e1d4cba86fd94d99f833cf29ca
Marek Fajkus <marek.faj@gmail.com>
2015-03-30 21:19:28 +0200

    improve install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,8 @@
-cwd=$(pwd)
+cwd="$(dirname "$0")"
+printf "$cwd \n"
 fpath=${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions
 
-ln -s $cwd/src/prompt_prague_shell_setup $fpath/prompt_prague_shell_setup
+ln -s $cwd/../src/prompt_prague_shell_setup $fpath/prompt_prague_shell_setup
 
 printf "Set :prezto:module:prompt in your .zpreztorc to prague_shell \n"
 

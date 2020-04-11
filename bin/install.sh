40d0fe5d3d76cd66aa1a941097181ad8f4fbdc84
Marek Fajkus <marek.faj@gmail.com>
2020-04-11 17:18:46 +0200

    update install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,11 @@
-cwd="$(dirname "$0")"
+#!/usr/bin/env bash
+set -e
+
+cwd="$(pwd)"
 printf "$cwd \n"
 fpath=${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions
 
-ln -s $cwd/../src/prompt_prague_shell_setup $fpath/prompt_prague_shell_setup
+ln -sf $cwd/src/prompt_prague_shell_setup $fpath/prompt_prague_shell_setup
 
 printf "Set :prezto:module:prompt in your .zpreztorc to prague_shell \n"
 
